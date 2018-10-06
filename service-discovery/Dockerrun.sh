f0550f1fbc3d3eccab2a1e64b5770852cefd329e
udalovas <al.udalec@gmail.com>
2018-10-06 11:10:36 +0100

    extract version into env var

--- service-discovery/Dockerrun.sh
+++ service-discovery/Dockerrun.sh
@@ -2,4 +2,4 @@
 echo "********************************************************"
 echo " Starting the Service Discovery (Eureka) Server"
 echo "********************************************************"
-java -Djava.security.egd=file:/dev/./urandom -jar /usr/local/eurekaserver/@project.build.finalName@.jar
+java -Djava.security.egd=file:/dev/./urandom -jar @project.build.finalName@.jar
